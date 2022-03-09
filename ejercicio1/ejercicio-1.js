e7907d7c05e21309908a4b43a1dcd1e8cfa1d2e4
Alonso Higareda <alonsohigareda23962@gmail.com>
2022-03-08 22:12:06 -0600

    ejercicios js

---
+++ ejercicio1/ejercicio-1.js
@@ -0,0 +1,24 @@
+
+//ejercicio 1
+var numero = Number(prompt("ingresa un numero"))
+var total = numero * 2
+console.log(total)
+
+//ejercicio 2
+var numero1 = Number(prompt("ingresa un numero"))
+var numero2 = Number(prompt("ingresa otro numero"))
+var total = numero1 - numero2
+console.log(total)
+
+// ejercicio 3
+var pi = 3.1416
+var r = 5.3 
+var total = pi * r ** 2
+console.log(total)
+
+
+//ejercicio 4 
+var p = 5 
+var D = 8
+var d= 6
+var total = D * d / 2
