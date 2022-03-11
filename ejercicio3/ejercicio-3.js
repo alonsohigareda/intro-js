b464c4560d48ebccf71fb1caff1ff5abe0f47402
Alonso Higareda <alonsohigareda23962@gmail.com>
2022-03-10 22:48:37 -0600

    agregue el ejercicio 4

--- ejercicio3/ejercicio-3.js
+++ ejercicio3/ejercicio-3.js
@@ -61,16 +61,16 @@ var numero3 = Number(prompt("ingresa un número más"))
     console.log(numero1)    
 
 
-} else if (numero2=numero3 && numero3<numero1) {
+} else if (numero2==numero3 && numero3<numero1) {
     console.log(numero1)
 
-}else if (numero2=numero1 && numero1<numero3) {
+}else if (numero2==numero1 && numero1<numero3) {
     console.log(numero3)
 
-}else if (numero1=numero2 && numero2<numero3) {
+}else if (numero1==numero2 && numero2<numero3) {
     console.log(numero3)
     
-}else if (numero1=numero3 && numero3<numero2) {
+}else if (numero1==numero3 && numero3<numero2) {
     console.log(numero2)
      
 }else   {
