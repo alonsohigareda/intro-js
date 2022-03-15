262346ca8ba3a1a97c4f36a6f179a93ff9018c63
Alonso Higareda <alonsohigareda23962@gmail.com>
2022-03-14 20:24:50 -0600

    subi ejercicios 5 y 6

---
+++ ejercicio5/ejercicio-5.js
@@ -0,0 +1,24 @@
+
+//ejercicio1
+
+// var arreglo = [1, 4, 6, 10, 22, 55, 46, 2, 5, 0]
+
+// for (var index = 0; index <= arreglo.length; index++) {
+    
+//     if (arreglo[index] > 3) {
+//         console.log(arreglo[index])
+//     }
+    
+// }
+
+
+//ejercicio2
+
+var arreglo = []
+var i = 0
+
+while (i<5) {
+    arreglo[i] = i
+    i++
+}
+
---
+++ ejercicio6/ejercicio-6.js
@@ -0,0 +1,32 @@
+
+//ejercicio1
+
+var animals = ["eagle", "parrot", "monkey", "boar", "lion"]
+
+var comingSoonAnimals = ["panter", "dragon", "turtle"]
+
+animals.concat(comingSoonAnimals)
+
+
+
+//ejercicio2
+
+var arr = [4, 6, 1, 0, 8, 2]
+
+arr.sort()
+
+
+
+//ejercicio3
+
+var animals = ["eagle", "parrot", "monkey", "boar", "lion"]
+
+animals.push("cow")
+
+
+
+//ejercicio4
+
+var animals = ["eagle", "parrot", "monkey", "boar", "lion"]
+
+animals.shift()
